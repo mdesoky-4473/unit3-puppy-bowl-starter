bb32275e547d1b535e3a801ea82ed54c486c3d12
Lester Lee <contact@lester-lee.com>
2024-09-30 14:30:41 -0400

    Reset to new Vite app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,3 @@
-// Import the React library, which allows us to define components
-
-
-// Import the Players component, which we'll use to show a list of players
-
-
-// Define the App component
-function App() {
-  // This component renders the Players component inside a div
-  // This div has a class of 'App', which we could use for styling
-  return (
-    
-  );
+export default function App() {
+  return <div>App</div>;
 }
-
-// Export the App component as the default export
-
